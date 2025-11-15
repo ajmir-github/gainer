92732262aee9b2c002e9dbc061fc56ca2fe84cb2
Ajmir Raziqi <ajmir.ng3@gmail.com>
2025-11-15 16:14:59 +0200

    add npm start

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-import netlify from "@netlify/vite-plugin";
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), netlify()],
+  plugins: [react(), tailwindcss()],
 });
